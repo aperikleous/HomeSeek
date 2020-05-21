ccdf94074bd5a4dc185db543821f7b1a4e51174c
Xenofontas Vlachogiannis <xenofonvlachogiannis@gmail.com>
2020-05-21 04:09:16 +0300

    All lot of changes to Places ( detail & index), Footer, NavBar, Layout

--- HomeSeek.Database/Migrations/Configuration.cs
+++ HomeSeek.Database/Migrations/Configuration.cs
@@ -118,11 +118,11 @@
             Reservation r3 = new Reservation() { CheckInDate = new DateTime(2020, 8, 21), CheckOutDate = new DateTime(2020, 8, 30), DaysOfStaying = 9, TotalAmount = 2290M,PaymentDate = new DateTime(2020, 7, 25), TotalFees = 229M };
 
             //================= Seeding Reviews =================
-            //Review template = new Review() { Title = "", Duration = 0, PhotoUrl = "", Price = 0, ProductionYear = new DateTime(1, 1, 1), Rating = 0D, TrailerUrl = "", Watched = false ,Country=Country.United_States_of_America};
-            Review rev1 = new Review() { Accuracy = 5, Checkin = 4, Cleanliness = 5, Location = 4, Value = 5, SubDate = new DateTime(2020, 6, 6), /*OverallRating = 9.1,*/ Comment = "ola teleia" };
-            Review rev2 = new Review() { Accuracy = 4, Checkin = 5, Cleanliness = 4, Location = 4, Value = 5, SubDate = new DateTime(2020, 7, 23),/* OverallRating = 9.1,*/ Comment = "apisteuto service" };
-            Review rev3 = new Review() { Accuracy = 3, Checkin = 4, Cleanliness = 3, Location = 5, Value = 5, SubDate = new DateTime(2020, 8, 31),/* OverallRating = 9.5,*/ Comment = "Super" };
-            Review rev4 = new Review() { Accuracy = 2, Checkin = 4, Cleanliness = 4, Location = 4, Value = 4, SubDate = new DateTime(2020, 8, 18),/* OverallRating = 9,*/ Comment = "SuperSuperSuperSuperSuperSuper" };
+            //Review template = new Review() { Accuracy = 5, Checkin = 4, Cleanliness = 5, Location = 4, Value = 5, SubDate = new DateTime(2020, 6, 6),  Comment = "ola teleia" };
+            Review rev1 = new Review() { Accuracy = 5, Checkin = 4, Cleanliness = 5, Location = 4, Value = 5, SubDate = new DateTime(2020, 6, 6),  Comment = "ola teleia" };
+            Review rev2 = new Review() { Accuracy = 4, Checkin = 5, Cleanliness = 4, Location = 4, Value = 5, SubDate = new DateTime(2020, 7, 23), Comment = "apisteuto service" };
+            Review rev3 = new Review() { Accuracy = 3, Checkin = 4, Cleanliness = 3, Location = 5, Value = 5, SubDate = new DateTime(2020, 8, 31), Comment = "Super" };
+            Review rev4 = new Review() { Accuracy = 2, Checkin = 4, Cleanliness = 4, Location = 4, Value = 4, SubDate = new DateTime(2020, 8, 18), Comment = "SuperSuperSuperSuperSuperSuper" };
 
             //================= Seeding Application User =================
             //ApplicationUser template = new ApplicationUser() { UserName = "", LastName = "", FirstName = "", City = "", DateOfBirth = new DateTime(1989, 08, 01) };
