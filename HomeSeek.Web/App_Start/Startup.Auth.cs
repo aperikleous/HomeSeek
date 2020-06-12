367071046b8b9f56a1365bdf243fc6004addc8aa
Than <Than@DESKTOP-GQVBVOF>
2020-06-12 23:07:45 +0300

    Without APIs

--- HomeSeek.Web/App_Start/Startup.Auth.cs
+++ HomeSeek.Web/App_Start/Startup.Auth.cs
@@ -50,36 +50,36 @@ namespace HomeSeek.Web
             app.UseTwoFactorRememberBrowserCookie(DefaultAuthenticationTypes.TwoFactorRememberBrowserCookie);
 
             // Uncomment the following lines to enable logging in with third party login providers
-            app.UseMicrosoftAccountAuthentication(
-                clientId: "590c160d-c1c9-4d42-9429-0f56ca9ac73a",
-                clientSecret: "KM9u.eYu3.vX4.TU48BndpIlQu9rUjR-bg");
+            //app.UseMicrosoftAccountAuthentication(
+            //    clientId: "",
+            //    clientSecret: "");
 
             //app.UseTwitterAuthentication(
             //   consumerKey: "",
             //   consumerSecret: "");
-            app.UseTwitterAuthentication(new TwitterAuthenticationOptions
-            {
-                ConsumerKey = "QSpEwiJtDv7v9jpEi7f477eZm",
-                ConsumerSecret = "8fclr1em0JmFlTzqay3Qh3knhEZUD3VcqpOglXQyupUvGTHU65",
-                BackchannelCertificateValidator = new CertificateSubjectKeyIdentifierValidator(new[] {
-                    "A5EF0B11CEC04103A34A659048B21CE0572D7D47", // VeriSign Class 3 Secure Server CA – G2
-                    "0D445C165344C1827E1D20AB25F40163D8BE79A5", // VeriSign Class 3 Secure Server CA – G3
-                    "7FD365A7C2DDECBBF03009F34339FA02AF333133", // VeriSign Class 3 Public Primary Certification Authority – G5
-                    "39A55D933676616E73A761DFA16A7E59CDE66FAD", // Symantec Class 3 Secure Server CA – G4
-                    "5168FF90AF0207753CCCD9656462A212B859723B", //DigiCert SHA2 High Assurance Server C‎A
-                    "B13EC36903F8BF4701D498261A0802EF63642BC3" //DigiCert High Assurance EV Root CA
-                    })
-            });
+            //app.UseTwitterAuthentication(new TwitterAuthenticationOptions
+            //{
+            //    ConsumerKey = "",
+            //    ConsumerSecret = "",
+            //    BackchannelCertificateValidator = new CertificateSubjectKeyIdentifierValidator(new[] {
+            //        "A5EF0B11CEC04103A34A659048B21CE0572D7D47", // VeriSign Class 3 Secure Server CA – G2
+            //        "0D445C165344C1827E1D20AB25F40163D8BE79A5", // VeriSign Class 3 Secure Server CA – G3
+            //        "7FD365A7C2DDECBBF03009F34339FA02AF333133", // VeriSign Class 3 Public Primary Certification Authority – G5
+            //        "39A55D933676616E73A761DFA16A7E59CDE66FAD", // Symantec Class 3 Secure Server CA – G4
+            //        "5168FF90AF0207753CCCD9656462A212B859723B", //DigiCert SHA2 High Assurance Server C‎A
+            //        "B13EC36903F8BF4701D498261A0802EF63642BC3" //DigiCert High Assurance EV Root CA
+            //        })
+            //});
 
-            app.UseFacebookAuthentication(
-               appId: "275152686973642",
-               appSecret: "b736e6da5722f9ea0210e02aef35cbc1");
+            //app.UseFacebookAuthentication(
+            //   appId: "",
+            //   appSecret: "");
 
-            app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
-            {
-                ClientId = "100533648788-eevu220jji011lhmcv615023ks8nchtp.apps.googleusercontent.com",
-                ClientSecret = "fRGbvcP0BTEx5-nJ3jwXzWKK"
-            });
+            //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
+            //{
+            //    ClientId = "",
+            //    ClientSecret = ""
+            //});
         }
     }
 }
